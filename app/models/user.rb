ef974aa358264cc6aae6f9d8a6bfa21506c407b7
NishikawaMasaki <masaki_re.re.re@icloud.com>
2020-09-08 14:09:05 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :text, presence: true
+ validates :nickname, presence: true
+ validates :email, presence: true, uniqueness: true, inclusion: { in: @ }
+ validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
+
+ validates :first_name, :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+ validates :first_name_kana, :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+ validates :birthday, presence: true
+
 end
