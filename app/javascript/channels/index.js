b6ba31cf209a7f218f001fd24ed2a952dc75fd75
NishikawaMasaki <masaki_re.re.re@icloud.com>
2020-09-15 21:28:30 +0900

    Javascript手数料と利益の修正

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function(){
+window.addEventListener('mouseover', function(){
   
   const price = document.getElementById("item-price")
   const commission = document.getElementById("add-tax-price")
@@ -8,7 +8,8 @@ price.addEventListener('change', function(){
   let item_price = price.value;
   let item_commission = Math.floor(item_price * 0.1)
   let item_profit = item_price - item_commission
- 
+  console.log(item_commission)
+  console.log(item_profit)
   commission.innerHTML = item_commission
   profit.innerHTML = item_profit
 })
